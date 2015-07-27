0a76ad2f77b6ca21b6fec5404ddca93643badb0c
adamrotmil <adam.rotmil@gmail.com>
2015-07-26 20:15:15 -0400

    revised checkbox functionality

--- js/app.js
+++ js/app.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
 		console.log("Hid the input box and list items")
 		$('.ascii').show();
 		console.log("Show the ASCII art")
-		$('.shop').toggleClass( "active" );
-		$('.credits').toggleClass( "active" );
+		$('.shop').toggleClass("active");
+		$('.credits').toggleClass("active");
 	});
 	$(".shop").mousedown(function() {
 		console.log("Clicked on shop");
@@ -24,49 +24,87 @@ $(document).ready(function() {
 		$('.list-items-list').show();
 		$('.logo').show();
 		console.log("Show the input box")
-		$('.shop').toggleClass( "active" );
-		$('.credits').toggleClass( "active" );
+		$('.shop').toggleClass("active");
+		$('.credits').toggleClass("active");
 	});
 
+	// APPEND LIST
+
 	$('input').keydown(function() {
-		// var myValue = $(this).val();
-		// append value of input as li in ul
-		if ( event.which == 13 ) {
+
+
+		if (event.which == 13) {
 			$(".list-items-list").append(
-				$('<li>').append(
-					$(this).val()
-			));
+				$('<li><button class="unchecked"></button>' + $('input')
+					.val() + '</li>'));
 		};
 
-	$('input').keydown(function(e) {		
+
+
+		$('input').keydown(function(e) {
 			console.log("A key was pressed.");
-			if ( event.which == 13 ) {
-			$('input').val(" ");
-			e.preventDefault();
+			if (event.which == 13) {
+				$('input').val(" ");
+				e.preventDefault();
 			}
 		});
- 	});
+	});
 
- 	$('input').mousedown(function() {
-			console.log("Mouse is down!");
-			$('input').val(" ");
- 	});
-	
+
+
+	$('input').mousedown(function() {
+		console.log("Mouse is down!");
+		$('input').val(" ");
+	});
+
 });
-
-
 
 // CLEARABLE INPUT
 //
 //
 
-function tog(v){return v?'addClass':'removeClass';} 
-$(document).on('input', '.clearable', function(){
-    $(this)[tog(this.value)]('x');
-}).on('mousemove', '.x', function( e ){
-    $(this)[tog(this.offsetWidth-20 < e.clientX-this.getBoundingClientRect().left)]('onX');   
-}).on('touchstart click', '.onX', function( ev ){
-    ev.preventDefault();
-    $(this).removeClass('x onX').val('').change();
+function tog(v) {
+	return v ? 'addClass' : 'removeClass';
+}
+$(document).on('input', '.clearable', function() {
+	$(this)[tog(this.value)]('x');
+}).on('mousemove', '.x', function(e) {
+	$(this)[tog(this.offsetWidth - 20 < e.clientX - this.getBoundingClientRect().left)]('onX');
+}).on('touchstart click', '.onX', function(ev) {
+	ev.preventDefault();
+	$(this).removeClass('x onX').val('').change();
 });
 
+
+
+// CLEARABLE LIST ITEMS
+//
+//
+$(document).on('click', 'li', function() {
+	console.log("clicked a list item")
+	$(this).closest('li').toggleClass('cleared');
+	$(this).children('button').toggleClass('unchecked checked');
+});
+
+$(document).on('mouseover', 'li', function() {
+	console.log("mouseover li")
+	$(this).closest('li').toggleClass('hover');
+});
+$(document).on('mouseout', 'li', function() {
+	$(this).closest('li').toggleClass('hover');
+});
+
+
+
+// *** BUGS
+//
+//
+// ** OK ** multiple columns in list causes jumping
+// ** OK ** multiple cols. in list when clicked cuts word in half
+// ** OK ** list item when clicked, append next item is duplicated
+//
+// 
+
+
+
+//
